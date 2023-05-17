a67586f8486913166084517ac73a63f82aba81dc
as247 <as247@vui360.com>
2023-05-18 00:03:05 +0700

    Kernel able to run without wordpress

--- src/WpStarter/Wordpress/Application.php
+++ src/WpStarter/Wordpress/Application.php
@@ -11,7 +11,7 @@ class Application extends \WpStarter\Foundation\Application
      *
      * @var string
      */
-    const VERSION = '1.4.8';
+    const VERSION = '1.4.10';
 
     protected $bootstrappedList = [];
 
