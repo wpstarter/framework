5628c81a30537bfbb6358e1b3a2bc3721b4ba4e8
as247 <as247@vui360.com>
2023-05-18 00:10:57 +0700

    Kernel return response if running outside WordPress

--- src/WpStarter/Wordpress/Application.php
+++ src/WpStarter/Wordpress/Application.php
@@ -11,7 +11,7 @@ class Application extends \WpStarter\Foundation\Application
      *
      * @var string
      */
-    const VERSION = '1.4.10';
+    const VERSION = '1.4.11';
 
     protected $bootstrappedList = [];
 
