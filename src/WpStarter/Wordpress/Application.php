42a6eeeb39a7a9c85f6cc8e0b138fd64ae984bac
as247 <as247@vui360.com>
2023-07-08 10:18:35 +0700

    1.6.10 ResourceManager

--- src/WpStarter/Wordpress/Application.php
+++ src/WpStarter/Wordpress/Application.php
@@ -11,7 +11,7 @@ class Application extends \WpStarter\Foundation\Application
      *
      * @var string
      */
-    const VERSION = '1.6.9';
+    const VERSION = '1.6.10';
 
     protected $bootstrappedList = [];
 
