a44cd45c42a5fd686729f5c6a578e7dc20ace85a
as247 <as247@vui360.com>
2023-07-14 09:42:11 +0700

    Admin route fix issue children have same slug with parent

--- src/WpStarter/Wordpress/Admin/Routing/MenuCollection.php
+++ src/WpStarter/Wordpress/Admin/Routing/MenuCollection.php
@@ -6,5 +6,14 @@ use WpStarter\Routing\RouteCollection;
 
 class MenuCollection extends RouteCollection
 {
+    protected function addToCollections($route)
+    {
+        $domainAndUri = $route->getDomain().$route->getAction('parent').$route->uri();
 
+        foreach ($route->methods() as $method) {
+            $this->routes[$method][$domainAndUri] = $route;
+        }
+
+        $this->allRoutes[$method.$domainAndUri] = $route;
+    }
 }
