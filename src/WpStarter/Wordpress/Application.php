1e8ed71bd016b7c425e068b82464ffaf4ac16c5c
as247 <as247@vui360.com>
2023-11-08 15:11:38 +0700

    v1.9.2

--- src/WpStarter/Wordpress/Application.php
+++ src/WpStarter/Wordpress/Application.php
@@ -11,7 +11,7 @@ class Application extends \WpStarter\Foundation\Application
      *
      * @var string
      */
-    const VERSION = '1.9.1';
+    const VERSION = '1.9.2';
 
     protected $bootstrappedList = [];
 
